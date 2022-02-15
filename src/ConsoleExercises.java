cf2242f4ed7a48a6e193bf25f81904e49747a3e1
Cody Hodges <hodges.cody00@gmail.com>
2022-02-15 15:59:03 -0600

    IO Exercises

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,39 +1,46 @@
 import java.util.Scanner;
 public class ConsoleExercises {
     public static void main(String[] args){
-        double pi = 3.14159;
-        System.out.printf("The value of pi is approximately %.2f \n", pi);
-
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.printf("Enter number: ");
-        int userNumber = scanner.nextInt();
-        System.out.println("Youe entered: " + userNumber);
-
-
-        Scanner scanner2 = new Scanner(System.in);
-        System.out.printf("Enter three words: ");
-        String word1 = scanner2.next();
-        String word2 = scanner2.next();
-        String word3 = scanner2.next();
-        System.out.println("First word: " +word1+ ", Second word: " +word2+ ", Third word: " +word3);
-
-        Scanner scanner3 = new Scanner(System.in);
-        System.out.printf("Enter sentence: ");
-        String sentence = scanner3.nextLine();
-        System.out.println("Sentence: " + sentence);
+//        double pi = 3.14159;
+//        System.out.printf("The value of pi is approximately %.2f \n", pi);
+//
+//
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.printf("Enter number: ");
+//        int userNumber = scanner.nextInt();
+//        System.out.println("Youe entered: " + userNumber);
+//
+//
+//        Scanner scanner2 = new Scanner(System.in);
+//        System.out.printf("Enter three words: ");
+//        String word1 = scanner2.next();
+//        String word2 = scanner2.next();
+//        String word3 = scanner2.next();
+//        System.out.println("First word: " +word1+ ", Second word: " +word2+ ", Third word: " +word3);
+//
+//        Scanner scanner3 = new Scanner(System.in);
+//        System.out.printf("Enter sentence: ");
+//        String sentence = scanner3.nextLine();
+//        System.out.println("Sentence: " + sentence);
 
         Scanner classroom = new Scanner(System.in);
-        System.out.printf("Enter width then height: ");
+
+        System.out.print("Enter width, length then height: ");
         int width = classroom.nextInt();
+        int length = classroom.nextInt();
         int height = classroom.nextInt();
-        System.out.println("Width: " + width + " Height: " + height);
+        System.out.printf("Width: %d, Length: %d, Height: %d%n", width, length, height);
 
         int area = width * height;
-        System.out.println("Area: " + area);
+        System.out.printf("Area: %d%n", area);
 
         int perimeter = (width * 2) + (height * 2);
-        System.out.printf("Perimeter: " + perimeter);
+        System.out.printf("Perimeter: %d%n", perimeter);
+
+        int volume = length * width * height;
+        System.out.printf("Volume: %d%n", volume);
+
+
 
     }
 }
