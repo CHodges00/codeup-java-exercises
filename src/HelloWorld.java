07a2e61445221be389cde3cdebaa07b24fe9bd70
Cody Hodges <hodges.cody00@gmail.com>
2022-02-15 11:54:10 -0600

    SDV Exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -17,50 +17,60 @@ public class HelloWorld {
         System.out.print('l');
         System.out.print('d');
         System.out.println('!');
+        System.out.println("---------------------------------");
 
         int myFavoriteNumber = 25;
         System.out.println(myFavoriteNumber);
+        System.out.println("---------------------------------");
 
         String myString = "Turtles";
         System.out.println(myString);
 //        Character myString = 'Turtles';
 //        int myString = "Turtles";
+        System.out.println("---------------------------------");
 
         double myNumber = 3.14;
         System.out.println(myNumber);
+        System.out.println("---------------------------------");
 
-//        int x = 5;
-//        System.out.println(x++);
-//        System.out.println(x);
-//
-//        int x = 5;
-//        System.out.println(++x);
-//        System.out.println(x);
+        int x = 5;
+        System.out.println(x++);
+        System.out.println(x);
+        System.out.println("---------------------------------");
+
+        x = 5;
+        System.out.println(++x);
+        System.out.println(x);
+        System.out.println("---------------------------------");
 
 //        String theNumberThree = "three";
 //        Object o = theNumberThree;
 //        int three = (int) "three";
 
-        int x = 4;
+        x = 4;
         x += 5;
         System.out.println(x);
+        System.out.println("---------------------------------");
 
         x = 3;
         int y;
         y = 4;
         y = y*=x;
         System.out.println(y);
+        System.out.println("---------------------------------");
 
 
         x = 10;
         y = 2;
         x = x/=y;
         System.out.println(x);
+        System.out.println("---------------------------------");
 
         x = 10;
         y = 2;
         y = y-=x;
         System.out.println(y);
+        System.out.println("---------------------------------");
 
 
 
@@ -70,6 +80,7 @@ public class HelloWorld {
         System.out.println(numbers++);
         System.out.println(numbers++);
         System.out.println(numbers++);
+        System.out.println("---------------------------------");
 
 
     }
