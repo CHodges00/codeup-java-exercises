9149d718068081ad21625ac4f0f27f907fbbe07e
Cody Hodges <hodges.cody00@gmail.com>
2022-02-17 08:24:30 -0600

    Control Exercises

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,2 +1,18 @@
 public class ControlFlowExercises {
+    public static void main(String[] args) {
+//        WHILE LOOP
+//        int i = 5;
+//        while (i <= 15){
+//            System.out.printf("%s ", i);
+//            i++;
+//        }
+
+
+        //            DO WHILE LOOP
+        int i = 0;
+        do {
+            System.out.printf("%s%n", i);
+
+        }while ();
+    }
 }
