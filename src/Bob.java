4ecc65272b82bb532331096bab384e3a32563c24
Cody Hodges <hodges.cody00@gmail.com>
2022-02-17 12:05:08 -0600

    Bob Exercise

--- src/Bob.java
+++ src/Bob.java
@@ -1,9 +1,28 @@
 import java.util.Scanner;
+
 public class Bob {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-        String entered = input.next();
         System.out.println("Let's Talk !");
+        String entered = input.nextLine();
+
+
+
+        String question = "Sure";
+        String yell = "Whoa, chill out !";
+        String nothing = "Fine. Be that way!";
+        String other = "Whatever";
+
+        if (entered.endsWith("?")) {
+            System.out.printf("%s", question);
+        } else if (entered.endsWith("!")) {
+            System.out.printf("%s", yell);
+        } else if (entered.equals("")) {
+            System.out.printf("%s", nothing);
+        } else {
+            System.out.printf("%s", other);
+        }
+
     }
 }
