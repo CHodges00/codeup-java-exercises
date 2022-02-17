c1975ca8884be3295f14aafd05e5a85d2baa9b15
Cody Hodges <hodges.cody00@gmail.com>
2022-02-17 09:37:49 -0600

    Control Exercises

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
 public class ControlFlowExercises {
     public static void main(String[] args) {
-//        WHILE LOOP
+
+
+        //        WHILE LOOP
+
 //        int i = 5;
 //        while (i <= 15){
 //            System.out.printf("%s ", i);
@@ -9,17 +13,20 @@ public class ControlFlowExercises {
 
 
         //            DO WHILE LOOP
+
 //        int i = 0;
 //        do {
 //            System.out.printf("%s%n", i);
 //            i = i + 2;
 //        }while (i <= 100);
 
+
 //        int i = 100;
 //        do {
 //            System.out.printf("%s%n", i);
 //            i = i - 5;
 //        } while (i >= -10);
+
 
 //        int i = 2;
 //        long i_lng = i;
@@ -28,7 +35,50 @@ public class ControlFlowExercises {
 //            i_lng = i_lng * i_lng;
 //        }while (i_lng < 1000000);
 
-//        REFACTOR TO FOR LOOP
+
+        //        REFACTOR TO FOR LOOP
+
+//        for (int i = 0; i <= 100; i += 2) {
+//            System.out.printf("%s%n", i);
+//        }
+
+
+//        for (int i = 100; i >= -10; i -= 5){
+//            System.out.printf("%s%n", i);
+//        }
+
+
+//        for (long i = 2; i < 1000000; i *= i) {
+//            System.out.printf("%s%n", i);
+//        }
+
+
+
+        //        FIZZBUZZ
+// 1 - 100
+//%3 = FIZZ
+//%5 = BUZZ
+//IF BOTH THEN = FIZZBUZZ
+//
+//
+//        for(int i = 1; i <= 100; i++) {
+//            if (i % 3 == 0 && i % 5 == 0) {
+//            System.out.println("FIZZBUZZ");
+//            } else if (i % 5 == 0){
+//            System.out.println("BUZZ");
+//            }else if (i % 3 == 0){
+//                System.out.println("FIZZ");
+//            }else{
+//                System.out.printf("%s%n", i);
+//            }
+//        }
+
+
+
+//        TABLE OF POWERS
+
+
+
 
 
 
