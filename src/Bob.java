f1ae147ce0953bef03d96c2ae7f6ba9a5d55d212
Cody Hodges <hodges.cody00@gmail.com>
2022-02-17 11:20:43 -0600

    Bob Exercise

---
+++ src/Bob.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Bob {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        String entered = input.next();
+        System.out.println("Let's Talk !");
+    }
+}
