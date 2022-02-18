986af7f4e1221cc4f4018a0cdc9f9b56417afb05
Cody Hodges <hodges.cody00@gmail.com>
2022-02-18 09:31:31 -0600

    HighLow Exercise

--- src/HighLow.java
+++ src/HighLow.java
@@ -1,19 +1,27 @@
 import  java.util.Scanner;
 import java.lang.Math;
-public class HighLow {
-    public static void main(String[] args) {
-        Scanner guess = new Scanner(System.in);
 
+
+public class HighLow {
+
+    public static void main(String[] args) {
+
+        Scanner guess = new Scanner(System.in);
         int count = 1;
+
+//GENERATE RANDOM #        
+//SET DOUBLE TO INT TO COMPARE TO USER INPUT
         double randomNum = Math.random();
         int randomNum_int = (int) (randomNum * 100) + 1;
         System.out.printf("Random number is: %s%n", randomNum_int);
 
+//START GUESS AND SHOW COUNT
         do {
             System.out.println("Guess the number between 1-100");
             System.out.printf("Guess #%s of 10%n", count);
             int userGuess = guess.nextInt();
 
+//PRINTS BASED ON GUESSES
             if (userGuess < randomNum_int){
                 System.out.println("Too low Joe");
             }else if (userGuess > randomNum_int){
@@ -21,14 +29,14 @@ public class HighLow {
             }else {
                 System.out.println("GOOD GUESS");
             }
-
+// INCREMENT COUNTER
             count++;
+
+//STOP GAME AFTER 10 TRIES
             if (count == 11){
                 System.out.println("Game Over 🤡");
-                System.out.println("Try again ? Y/N");
             }
-
-
+// WHILE 10 OR LESS, CONTINUE RUNNING
         }while (count <= 10);
 
     }
