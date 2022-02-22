26090b852f9f95bcf7102a6740d6a8bca7eaa2b3
Cody Hodges <hodges.cody00@gmail.com>
2022-02-22 12:05:43 -0600

    Environment

---
+++ src/LectureEnvironment.java
@@ -0,0 +1,14 @@
+public class LectureEnvironment{
+
+    public static void main(String[] args) {
+
+        Person thisPerson = new Person();
+
+        thisPerson.first = "Sam";
+        thisPerson.last = "Clemens";
+
+        System.out.println(thisPerson.sayHello());
+
+    }
+
+}
