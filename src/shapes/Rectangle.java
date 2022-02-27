6a01c6d00ba12b140f0f36370bd3f919e5dc6d6d
Cody Hodges <hodges.cody00@gmail.com>
2022-02-27 15:36:44 -0600

    Shapes Exercise

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -3,24 +3,5 @@ package shapes;
 class Rectangle {
 
 
-    public int width;
-    public int length;
 
-
-    public Rectangle(int width, int length){
-        this.width = width;
-        this.length = length;
-
-    }
-
-
-
-
-    public int getPerimeter(){
-        return (2* this.width)+(2* this.length);
-    }
-
-    public int getArea() {
-        return this.length * this.width;
-    }
 }
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -3,23 +3,5 @@ package shapes;
 
 class Square extends Rectangle{
 
-    public int side;
 
-    public Square(int side) {
-        super(side, side);
-        this.side = side;
-
-    }
-
-
-    public int getArea() {
-        System.out.println("OVERRIDDEN");
-        return super.getArea();
-    }
-
-
-    public int getPerimeter() {
-        System.out.println("OVERRIDDEN");
-        return super.getPerimeter();
-    }
 }
