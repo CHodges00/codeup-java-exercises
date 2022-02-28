5dcf8d033afe95b72d301c51ceeec0aba85a3118
Cody Hodges <hodges.cody00@gmail.com>
2022-02-28 17:37:30 -0600

    Arrays Exercise

---
+++ src/movies/Movie.java
@@ -0,0 +1,25 @@
+package movies;
+
+public class Movie {
+
+    private String name;
+    private String category;
+
+//    GETTERS
+    public String getName() {
+        return name;
+    }
+    public String getCategory() {
+        return category;
+    }
+
+//    SETTERS
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+
+}
---
+++ src/movies/MoviesApplication.java
@@ -0,0 +1,16 @@
+package movies;
+import java.util.Scanner;
+
+public class MoviesApplication {
+
+    Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        
+
+
+    }
+
+
+}
