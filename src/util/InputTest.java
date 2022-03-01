949b224c4b3e47ac7d737f5d12e77c97a7b5e06e
Cody Hodges <hodges.cody00@gmail.com>
2022-03-01 10:05:40 -0600

    Input Exercises:

--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -5,24 +5,25 @@ public class InputTest {
     public static void main(String[] args) {
 
         Input input = new Input();
+//
+////        RETURNS STRING ENTRY
+//        System.out.println(input.getString());
+//
+////        RETURNS BOOLEAN OF Y/N ENTRY
+//        System.out.println(input.yesNo());
+//
+////        RETURNS NUMBER IF ENTERED BETWEEN MIN/MAX
+//        System.out.println(input.getInt(1, 10));
+//
+////        RETURNS INT ENTERED
+//        System.out.println(input.getInt());
+//
+////        RETURNS DOUBLE IF ENTERED BETWEEN MIN/MAX
+//        System.out.println(input.getDouble(1.1, 10.1));
+//
+////        RETURNS DOUBLE ENTERED
+//        System.out.println(input.getDouble());
+//    }
 
-//        RETURNS STRING ENTRY
-        System.out.println(input.getString());
-
-//        RETURNS BOOLEAN OF Y/N ENTRY
-        System.out.println(input.yesNo());
-
-//        RETURNS NUMBER IF ENTERED BETWEEN MIN/MAX
-        System.out.println(input.getInt(1, 10));
-
-//        RETURNS INT ENTERED
-        System.out.println(input.getInt());
-
-//        RETURNS DOUBLE IF ENTERED BETWEEN MIN/MAX
-        System.out.println(input.getDouble(1.1, 10.1));
-
-//        RETURNS DOUBLE ENTERED
-        System.out.println(input.getDouble());
-    }
-
+}
 }
