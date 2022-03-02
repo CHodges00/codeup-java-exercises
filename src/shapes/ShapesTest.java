e0c5665f441997496f82fcefd18422d53b97e3b3
Cody Hodges <hodges.cody00@gmail.com>
2022-03-02 10:38:37 -0600

    Shapes Exercises

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -4,11 +4,7 @@ public class ShapesTest {
 
 Measurable myShape;
 
-    public static void main(String[] args) {
-        Rectangle shape1 = new Rectangle();
-        shape1.setting(5,5);
-        shape1.getArea();
-    }
+
 
 
 }
