bcd87c6bfc362e487432f785036642f5b7456dcb
Cody Hodges <hodges.cody00@gmail.com>
2022-03-26 15:18:51 -0500

    Practice

--- src/ServletPractice.java
+++ src/ServletPractice.java
@@ -1,2 +1,13 @@
-public class ServletPractice {
+public class ServletPractice{
+
+//    A servlet is a Java class that extends the HttpServlet class from the servlet library.
+//    A servlet's most basic functionality is to handle HTTP requests and responses.
+//    Servlets also allow us to create dynamic web pages with Java;
+//    that is, we can create HTML responses where the content changes based on variables, not just static HTML pages.
+
+
+
+
+
+
 }
---
+++ src/learn.java
@@ -0,0 +1,24 @@
+public class learn {
+
+    public static void main(String[] args) {
+
+        int y = 1;
+        System.out.println("y = " + y);
+
+        boolean yes = true;
+        boolean no = false;
+        System.out.println("yes = " + yes);
+        System.out.println("no = " + no);
+
+        String str = "New String";
+        System.out.println("str = " + str);
+
+
+
+
+
+
+
+    }
+}
+
