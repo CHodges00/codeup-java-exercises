b5929253af1ad6b5d18291a8420f6afb93554634
Cody Hodges <hodges.cody00@gmail.com>
2022-03-26 15:30:41 -0500

    Practice

--- src/learn.java
+++ src/learn.java
@@ -6,19 +6,24 @@ public class learn {
 
     public static void main(String[] args) {
 
+//        CREATE INT VARIABLE
         int y = 1;
         System.out.println("y = " + y);
 
+//        BOOLEAN VALUES
         boolean yes = true;
         boolean no = false;
         System.out.println("yes = " + yes);
         System.out.println("no = " + no);
 
+//        GENERAGTE NEW STR VARIABLE
         String str = "New String";
         System.out.println("str = " + str);
 
+//        RUN MULT METHOD
         System.out.println("showMult(5, 5) = " + showMult(5, 5));
 
+//        PRINT STR 5 TIMES
         for (int i = 0; i < 5; i++){
             System.out.println("str = " + str);
         }
