4e8c7adc43f2310f67cb521992028d0e7f9d4a19
Cody Hodges <hodges.cody00@gmail.com>
2022-07-27 23:44:00 -0400

    Java practice

--- src/solution.java
+++ src/solution.java
@@ -115,3 +115,36 @@ import java.util.Scanner;
 //        }
 //    }
 //}
+
+
+
+
+// Write your code here
+//
+//static boolean flag = false;
+//
+//static int B;
+//static int H;
+//
+//static{
+//        Scanner in = new Scanner(System.in);
+//        B=in.nextInt();
+//        H=in.nextInt();
+//        if(B<=0 || H <=0){
+//        System.out.println("java.lang.Exception: Breadth and height must be positive");
+//        }else{
+//        flag = true;
+//        }
+//        }
+//
+//
+//
+//public static void main(String[] args){
+//        if(flag){
+//        int area=B*H;
+//        System.out.print(area);
+//        }
+//
+//        }//end of main
+//
+//        }//end of class
