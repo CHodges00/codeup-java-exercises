73c291f54ff9e7ea42c708df3084aa4cbce36fcb
Cody Hodges <hodges.cody00@gmail.com>
2022-07-27 23:48:48 -0400

    Java practice

--- src/solution.java
+++ src/solution.java
@@ -1,3 +1,4 @@
+import java.security.Permission;
 import java.util.Scanner;
 //public class solution {
 //
@@ -118,7 +119,7 @@ import java.util.Scanner;
 
 
 
-
+//If both values are greater than zero, then the main method must output the area of the parallelogram. Otherwise, print "java.lang.Exception: Breadth and height must be positive" without quotes.
 // Write your code here
 //
 //static boolean flag = false;
@@ -148,3 +149,53 @@ import java.util.Scanner;
 //        }//end of main
 //
 //        }//end of class
+
+
+
+//If n = String
+//public class Solution {
+//    public static void main(String[] args) {
+//
+//        DoNotTerminate.forbidExit();
+//
+//        try {
+//            Scanner in = new Scanner(System.in);
+//            int n = in .nextInt();
+//            in.close();
+//            //String s=???; Complete this line below
+//
+//            //Write your code here
+//
+//            String s = String.valueOf(n);
+//
+//            if (n == Integer.parseInt(s)) {
+//                System.out.println("Good job");
+//            } else {
+//                System.out.println("Wrong answer.");
+//            }
+//        } catch (DoNotTerminate.ExitTrappedException e) {
+//            System.out.println("Unsuccessful Termination!!");
+//        }
+//    }
+//}
+//
+////The following class will prevent you from terminating the code using exit(0)!
+//class DoNotTerminate {
+//
+//    public static class ExitTrappedException extends SecurityException {
+//
+//        private static final long serialVersionUID = 1;
+//    }
+//
+//    public static void forbidExit() {
+//        final SecurityManager securityManager = new SecurityManager() {
+//            @Override
+//            public void checkPermission(Permission permission) {
+//                if (permission.getName().contains("exitVM")) {
+//                    throw new ExitTrappedException();
+//                }
+//            }
+//        };
+//        System.setSecurityManager(securityManager);
+//    }
+//}
