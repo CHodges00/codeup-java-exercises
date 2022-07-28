52f1f1c88accaf369eb6bbc588d1870c011a96a3
Cody Hodges <hodges.cody00@gmail.com>
2022-07-27 23:09:43 -0400

    Java practice

--- src/solution.java
+++ src/solution.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 //public class solution {
 //
 //
@@ -53,6 +54,38 @@
 //                System.out.print(getValue(a,b,j)+" ");
 //            }
 //            System.out.println();
+//        }
+//    }
+//}
+
+
+//class Solution{
+//    public static void main(String []argh)
+//    {
+//
+//
+//
+//        Scanner sc = new Scanner(System.in);
+//        int t=sc.nextInt();
+//
+//        for(int i=0;i<t;i++)
+//        {
+//
+//            try
+//            {
+//                long x=sc.nextLong();
+//                System.out.println(x+" can be fitted in:");
+//                if(x>=-128 && x<=127)System.out.println("* byte");
+//                //Complete the code
+//                if(x == (short)x)System.out.println("* short");
+//                if(x == (int)x)System.out.println("* int");
+//                if(x == (long)x)System.out.println("* long");
+//            }
+//            catch(Exception e)
+//            {
+//                System.out.println(sc.next()+" can't be fitted anywhere.");
+//            }
+//
 //        }
 //    }
 //}
