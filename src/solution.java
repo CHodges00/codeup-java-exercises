111a54dfb59447c77fe5cba048abaf8e1d425b95
Cody Hodges <hodges.cody00@gmail.com>
2022-08-01 11:44:51 -0400

    Daily HackerRank

--- src/solution.java
+++ src/solution.java
@@ -1,6 +1,8 @@
 import java.security.Permission;
 import java.text.NumberFormat;
 import java.util.Locale;
+import java.io.*;
+import java.util.*;
 import java.util.Scanner;
 //public class solution {
 //
@@ -236,3 +238,23 @@ import java.util.Scanner;
 //        System.out.println("France: " + france);
 //    }
 //}
+
+
+
+
+//Given a string, , matching the regular expression [A-Za-z !,?._'@]+,
+//        split the string into tokens. We define a token to be one or more consecutive English alphabetic letters.
+//        Then, print the number of tokens, followed by each token on a new line.
+
+public class solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        // Write your code here.
+
+        scan.close();
+        System.out.println(s);
+
+    }
+}
