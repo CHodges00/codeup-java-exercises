bc3c6c11ed95654550dddc0ae754d0db3d386ec4
Cody Hodges <hodges.cody00@gmail.com>
2022-08-25 16:19:38 -0500

    HackerRank Practice

--- src/solution.java
+++ src/solution.java
@@ -523,16 +523,55 @@ class Result {*/
 //  Otherwise, print Invalid. Do not print the quotes.
 
 
-public class solution
-{
-    public static void main(String[] args){
-        Scanner in = new Scanner(System.in);
-        int testCases = Integer.parseInt(in.nextLine());
-        while(testCases>0){
-            String pattern = in.nextLine();
-            //Write your code
-            System.out.println(pattern);
-            testCases--;
+//public class solution
+//{
+//    public static void main(String[] args){
+//        Scanner in = new Scanner(System.in);
+//        int testCases = Integer.parseInt(in.nextLine());
+//        while(testCases>0){
+//            String pattern = in.nextLine();
+//            //Write your code
+//            System.out.println(pattern);
+//            testCases--;
+//        }
+//    }
+//}
+
+
+
+
+
+//        You are updating the username policy on your company's internal networking platform. According to the policy,
+//        a username is considered valid if all the following constraints are satisfied:
+//
+//    The username consists of  to  characters inclusive. If the username consists of less than  or greater than  characters, then it is an invalid username.
+//    The username can only contain alphanumeric characters and underscores (_). Alphanumeric characters describe the character set consisting of lowercase characters,
+//      uppercase characters , and digits .
+//    The first character of the username must be an alphabetic character, i.e., either lowercase character  or uppercase character .
+
+
+
+class UsernameValidator {
+    /*
+     * Write regular expression here.
+     */
+    public static final String regularExpression = null;
+}
+
+
+public class solution {
+    private static final Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = Integer.parseInt(scan.nextLine());
+        while (n-- != 0) {
+            String userName = scan.nextLine();
+
+            if (userName.matches(UsernameValidator.regularExpression)) {
+                System.out.println("Valid");
+            } else {
+                System.out.println("Invalid");
+            }
         }
     }
 }
