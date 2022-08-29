d98cfc90e2355d4b0138a0185b8aaa30c4b19d03
Cody Hodges <hodges.cody00@gmail.com>
2022-08-28 22:19:19 -0500

    HackerRank Practice

--- src/solution.java
+++ src/solution.java
@@ -678,3 +678,38 @@ class Result {*/
 //    }
 //}
 
+
+
+
+
+
+
+
+
+//
+//        The first line will have an integer N denoting the number of entries in the phone book.
+//        Each entry consists of two lines: a name and the corresponding phone number.
+//          For each case, print "Not found" if the person has no entry in the phone book.
+//          Otherwise, print the person's name and phone number. See sample output for the exact format.
+
+
+class solution {
+    public static void main(String []argh) {
+        Scanner in = new Scanner(System.in);
+        int n=in.nextInt();
+        in.nextLine();
+        for(int i=0;i<n;i++)
+        {
+            String name=in.nextLine();
+            int phone=in.nextInt();
+            in.nextLine();
+        }
+        while(in.hasNext())
+        {
+            String s=in.nextLine();
+        }
+
+
+    }
+}
+
