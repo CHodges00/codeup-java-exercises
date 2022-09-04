6087202c27bde70495fb5ae855c776096d2d0a2e
Cody Hodges <hodges.cody00@gmail.com>
2022-09-03 22:52:10 -0500

    HackerRank Practice

--- src/solution.java
+++ src/solution.java
@@ -693,25 +693,25 @@ class Result {*/
 //          Otherwise, print the person's name and phone number. See sample output for the exact format.
 
 
-class solution{
-    public static void main(String []argh){
-        Scanner in = new Scanner(System.in);
-        int n=Integer.parseInt(in.nextLine());
-        HashMap<String,Integer> data = new HashMap<>();
-        for(int i=0;i<n;i++)
-        {
-            String name=in.nextLine();
-            int phone=Integer.parseInt(in.nextLine());
-            data.put(name, phone);
-        }
-        while(in.hasNext())
-        {
-            String s=in.nextLine();
-            System.out.println(data.containsKey(s)?s+"="+data.get(s):"Not found");
-
-        }
-    }
-}
+//class solution{
+//    public static void main(String []argh){
+//        Scanner in = new Scanner(System.in);
+//        int n=Integer.parseInt(in.nextLine());
+//        HashMap<String,Integer> data = new HashMap<>();
+//        for(int i=0;i<n;i++)
+//        {
+//            String name=in.nextLine();
+//            int phone=Integer.parseInt(in.nextLine());
+//            data.put(name, phone);
+//        }
+//        while(in.hasNext())
+//        {
+//            String s=in.nextLine();
+//            System.out.println(data.containsKey(s)?s+"="+data.get(s):"Not found");
+//
+//        }
+//    }
+//}
 
 
 
@@ -766,3 +766,26 @@ class solution{
 //    }
 //}
 
+
+
+
+
+
+
+//
+//        IP address is a string in the form "A.B.C.D",
+//        where the value of A, B, C, and D may range from 0 to 255.
+//        Leading zeros are allowed. The length of A, B, C, or D can't be greater than 3.
+
+
+class solution{
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+
+    }
+}
