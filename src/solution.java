e7e0bf9f7deb038a6f7414c7790af46b0d7bd703
Cody Hodges <hodges.cody00@gmail.com>
2022-09-04 16:03:52 -0500

    HackerRank Practice

--- src/solution.java
+++ src/solution.java
@@ -806,13 +806,20 @@ class Result {*/
 public class solution {
 
     public static String getSmallestAndLargest(String s, int k) {
-        String smallest = "";
-        String largest = "";
 
         // Complete the function
         // 'smallest' must be the lexicographically smallest substring of length 'k'
 
         // 'largest' must be the lexicographically largest substring of length 'k'
+        int n = s.length();
+        String smallest = s.substring(0,k);
+        String largest =s.substring(0,k);
+
+        for(int i = 0;i<n-k+1;i++){
+            String temp = s.substring(i,i+k);
+            largest = largest.compareTo(temp)>0?largest:temp;
+            smallest = smallest.compareTo(temp)<0?smallest:temp;
+        }
 
         return smallest + "\n" + largest;
     }
