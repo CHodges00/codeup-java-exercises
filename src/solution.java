d6c13d8fe2438f7dbd42018b13bada98643792de
Cody Hodges <hodges.cody00@gmail.com>
2022-09-08 22:31:13 -0500

    HackerRank Practice

--- src/solution.java
+++ src/solution.java
@@ -887,30 +887,31 @@ class Result {*/
 //        Write the two necessary arguments for replaceAll such that each repeated word is replaced with
 //        the very first instance the word found in the sentence. It must be the exact first occurrence of the word, as the expected output is case-sensitive.
 
-public class DuplicateWords {
-
-    public static void main(String[] args) {
-
-        String regex = "/* Write a RegEx matching repeated words here. */";
-        Pattern p = Pattern.compile(regex, /* Insert the correct Pattern flag here.*/);
-
-        Scanner in = new Scanner(System.in);
-        int numSentences = Integer.parseInt(in.nextLine());
-
-        while (numSentences-- > 0) {
-            String input = in.nextLine();
-
-            Matcher m = p.matcher(input);
-
-            // Check for subsequences of input that match the compiled pattern
-            while (m.find()) {
-                input = input.replaceAll(/* The regex to replace */, /* The replacement. */);
-            }
-
-            // Prints the modified sentence.
-            System.out.println(input);
-        }
-
-        in.close();
-    }
-}
+//public class solution {
+//
+//    public static void main(String[] args) {
+//
+//        String regex = "\\b(\\w+)(\\s+\\1)*\\b";
+//        Pattern p = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+//
+//        Scanner in = new Scanner(System.in);
+//        int numSentences = Integer.parseInt(in.nextLine());
+//
+//        while (numSentences-- > 0) {
+//            String input = in.nextLine();
+//
+//            Matcher m = p.matcher(input);
+//
+//            // Check for subsequences of input that match the compiled pattern
+//            while (m.find()) {
+//                input = input.replaceAll(m.group(),
+//                        m.group(1));
+//            }
+//
+//            // Prints the modified sentence.
+//            System.out.println(input);
+//        }
+//
+//        in.close();
+//    }
+//}
