47a676504917d0895b69f84d412f6fdf847742d7
Cody Hodges <hodges.cody00@gmail.com>
2022-09-10 15:57:00 -0500

    HackerRank Practice

--- src/solution.java
+++ src/solution.java
@@ -930,32 +930,93 @@ class Result {*/
 //        if no valid content is found, print None.
 
 
-public class solution{
-    public static void main(String[] args){
+//public class solution{
+//    public static void main(String[] args){
+//
+//        Scanner in = new Scanner(System.in);
+//        int testCases = Integer.parseInt(in.nextLine());
+//        while(testCases>0){
+//            String line = in.nextLine();
+//
+//            //Write your code here
+//            String regexPattern = "<(.+)>([\\w]+[^<]*)</(\\1)>";
+//            Pattern p = Pattern.compile(regexPattern);
+//            Matcher m = p.matcher(line);
+//            int count = 0;
+//
+//            while(m.find())
+//            {
+//                // System.out.println(m.group(0)); // get <h1>...</h1>
+//                // System.out.println(m.group(1)); // get the text inside the tag
+//                System.out.println(m.group(2)); // get the text between <x> </x>
+//                count++;
+//            }
+//            if(count == 0){
+//                System.out.println("None");
+//            }
+//
+//            testCases--;
+//        }
+//    }
+//}
 
-        Scanner in = new Scanner(System.in);
-        int testCases = Integer.parseInt(in.nextLine());
-        while(testCases>0){
-            String line = in.nextLine();
 
-            //Write your code here
-            String regexPattern = "<(.+)>([\\w]+[^<]*)</(\\1)>";
-            Pattern p = Pattern.compile(regexPattern);
-            Matcher m = p.matcher(line);
-            int count = 0;
 
-            while(m.find())
-            {
-                // System.out.println(m.group(0)); // get <h1>...</h1>
-                // System.out.println(m.group(1)); // get the text inside the tag
-                System.out.println(m.group(2)); // get the text between <x> </x>
-                count++;
+
+
+
+
+
+
+
+
+
+//        PerformOperation isOdd(): The lambda expression must return true if a number is odd or false if it is even.
+//        PerformOperation isPrime(): The lambda expression must return true if a number is prime or false if it is composite.
+//        PerformOperation isPalindrome(): The lambda expression must return true if a number is a palindrome or false if it is not.
+
+
+
+class MyMath {
+    public static boolean checker(PerformOperation p, int num) {
+        return p.check(num);
+    }
+
+    // Write your code here
+    static void isOdd() {
+
+    }
+
+    public class Solution {
+
+        public static void main(String[] args) throws IOException {
+            MyMath ob = new MyMath();
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int T = Integer.parseInt(br.readLine());
+            PerformOperation op;
+            boolean ret = false;
+            String ans = null;
+            while (T-- > 0) {
+                String s = br.readLine().trim();
+                StringTokenizer st = new StringTokenizer(s);
+                int ch = Integer.parseInt(st.nextToken());
+                int num = Integer.parseInt(st.nextToken());
+                if (ch == 1) {
+                    op = ob.isOdd();
+                    ret = ob.checker(op, num);
+                    ans = (ret) ? "ODD" : "EVEN";
+                } else if (ch == 2) {
+                    op = ob.isPrime();
+                    ret = ob.checker(op, num);
+                    ans = (ret) ? "PRIME" : "COMPOSITE";
+                } else if (ch == 3) {
+                    op = ob.isPalindrome();
+                    ret = ob.checker(op, num);
+                    ans = (ret) ? "PALINDROME" : "NOT PALINDROME";
+
+                }
+                System.out.println(ans);
             }
-            if(count == 0){
-                System.out.println("None");
-            }
-
-            testCases--;
         }
     }
 }
