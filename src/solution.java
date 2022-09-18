ae8196c50eefb703acade77ea27b156825ccdaaf
Cody Hodges <hodges.cody00@gmail.com>
2022-09-18 14:38:01 -0500

    HackerRank Practice

--- src/solution.java
+++ src/solution.java
@@ -1027,12 +1027,43 @@ import java.util.regex.*;
 //You have a test string S. Your task is to match the pattern xxXxxXxxxx
 //Here x denotes a digit character, and X denotes a non-digit character.
 
+//public class solution {
+//
+//    public static void main(String[] args) {
+//
+//        Regex_Test tester = new Regex_Test();
+//        tester.checker("(\\d{2}\\D){2}\\d{4}"); // Use \\ instead of using \
+//
+//    }
+//}
+//
+//class Regex_Test {
+//
+//    public void checker(String Regex_Pattern){
+//
+//        Scanner Input = new Scanner(System.in);
+//        String Test_String = Input.nextLine();
+//        Pattern p = Pattern.compile(Regex_Pattern);
+//        Matcher m = p.matcher(Test_String);
+//        System.out.println(m.find());
+//    }
+//
+//}
+
+
+
+
+
+
+//        You have a test string S. Your task is to match the pattern XXxXXxXX
+//        Here, x denotes whitespace characters, and X denotes non-white space characters.
+
 public class solution {
 
     public static void main(String[] args) {
 
         Regex_Test tester = new Regex_Test();
-        tester.checker("(\\d{2}\\D){2}\\d{4}"); // Use \\ instead of using \
+        tester.checker("(\\S{2}\\s){2}\\S{2}");
 
     }
 }
