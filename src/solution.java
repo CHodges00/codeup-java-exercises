73f49bcd4130a205380061193f1638ee9874050f
Cody Hodges <hodges.cody00@gmail.com>
2022-09-19 23:27:56 -0500

    Interview Prep

--- src/solution.java
+++ src/solution.java
@@ -1162,6 +1162,7 @@ import java.text.*;
 import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
+import java.util.stream.Stream;
 
 
 //class Result {
@@ -1204,17 +1205,69 @@ import java.util.regex.*;
 //    }
 //}
 
-public class solution {
+//public class solution {
+//
+//
+//    public static void main(String[] args) {
+//        String expr = "2x + 3";
+//        int val = 5;
+//        ArrayList<Integer> arr = new ArrayList<>();
+//        for(int i = 0; i < expr.length(); i++){
+//            arr.add(i);
+//        }
+//
+//        System.out.println(arr);
+//    }
+//}
 
 
-    public static void main(String[] args) {
-        String expr = "2x + 3";
-        int val = 5;
-        ArrayList<Integer> arr = new ArrayList<>();
-        for(int i = 0; i < expr.length(); i++){
-            arr.add(i);
+
+
+
+
+
+//        Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers.
+//        Then print the respective minimum and maximum values as a single line of two space-separated long integers.
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void miniMaxSum(List<Integer> arr) {
+        // Write your code here
+        long min = 0;
+        long max = 0;
+
+        Collections.sort(arr);
+        // System.out.println(arr);
+
+        for(int i=0;i<arr.size()-1;i++){
+            min += arr.get(i);
+        }
+        for(int i=1;i<arr.size();i++){
+            max += arr.get(i);
         }
 
-        System.out.println(arr);
+        System.out.println(min + " " + max);
+    }
+
+}
+
+public class solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        Result.miniMaxSum(arr);
+
+        bufferedReader.close();
     }
 }
