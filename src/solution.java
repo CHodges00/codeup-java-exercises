c197ba7ffbd54ed933ec14b3c73d28b73d3c3c75
Cody Hodges <hodges.cody00@gmail.com>
2022-09-20 23:35:44 -0500

    HackerRank Practice

--- src/solution.java
+++ src/solution.java
@@ -1162,6 +1162,7 @@ import java.text.*;
 import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 
@@ -1279,23 +1280,70 @@ import static java.util.stream.Collectors.toList;
 
 //Given an array of integers, where all elements but one occur twice, find the unique element.
 
+//class Result {
+//    public static int lonelyinteger(List<Integer> a) {
+//
+//        int uniq = 0;
+//
+//        for (int i=0; i<a.size(); i++){
+//            uniq = a.get(i);
+//            a.remove(i);
+//            if (!a.contains(uniq))
+//                return uniq;
+//            a.add(i, uniq);
+//        }
+//        return uniq;
+//    }
+//}
+//
+//
+//
+//public class solution {
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+//
+//        int n = Integer.parseInt(bufferedReader.readLine().trim());
+//
+//        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+//                .map(Integer::parseInt)
+//                .collect(toList());
+//
+//        int result = Result.lonelyinteger(a);
+//
+//        bufferedWriter.write(String.valueOf(result));
+//        bufferedWriter.newLine();
+//
+//        bufferedReader.close();
+//        bufferedWriter.close();
+//    }
+//}
+
+
+
+
+
+
+
+
+//Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+
+
 class Result {
-    public static int lonelyinteger(List<Integer> a) {
 
-        int uniq = 0;
+    /*
+     * Complete the 'diagonalDifference' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+     */
 
-        for (int i=0; i<a.size(); i++){
-            uniq = a.get(i);
-            a.remove(i);
-            if (!a.contains(uniq))
-                return uniq;
-            a.add(i, uniq);
-        }
-        return uniq;
+    public static int diagonalDifference(List<List<Integer>> arr) {
+        // Write your code here
+
     }
+
 }
-
-
 
 public class solution {
     public static void main(String[] args) throws IOException {
@@ -1304,11 +1352,21 @@ public class solution {
 
         int n = Integer.parseInt(bufferedReader.readLine().trim());
 
-        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
-                .map(Integer::parseInt)
-                .collect(toList());
+        List<List<Integer>> arr = new ArrayList<>();
 
-        int result = Result.lonelyinteger(a);
+        IntStream.range(0, n).forEach(i -> {
+            try {
+                arr.add(
+                        Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                                .map(Integer::parseInt)
+                                .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        int result = Result.diagonalDifference(arr);
 
         bufferedWriter.write(String.valueOf(result));
         bufferedWriter.newLine();
